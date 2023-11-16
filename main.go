31520532587e152cda9fecbb8ff35e54aa29f6fa
danh-mevn <nguyen.danh@marketenterprise.vn>
2023-11-16 13:08:09 +0700

    feat: update main.go

--- main.go
+++ main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/dawnnguyen-vn/learn-go/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	r := gin.Default()
+	if APP_ENV := os.Getenv("APP_ENV"); APP_ENV == "dev" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+	if APP_ENV := os.Getenv("APP_ENV"); APP_ENV != "dev" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	r := gin.New()
 
 	r.GET("/health", func(c *gin.Context) {
-		c.String(200, "okkk")
+		c.String(200, "ok")
 	})
 
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, utils.GetValue())
 	})
 
+	if APP_ENV := os.Getenv("APP_ENV"); APP_ENV != "dev" {
+		fmt.Print("Listening and serving HTTP on :" + os.Getenv("PORT"))
+	}
 	r.Run()
 }
